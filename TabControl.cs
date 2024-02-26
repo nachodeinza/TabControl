66bd3a06f32995b37dff1caba11f8b5bb0529349
nachodeinza <153104390+nachodeinza@users.noreply.github.com>
2024-02-26 08:20:41 +0100

    Update TabControl.cs

--- TabControl.cs
+++ TabControl.cs
@@ -1,8 +1,27 @@
-tabControl1.TabPages.Add("Pestanya 1");
-tabControl1.TabPages.Add("Pestanya 2");
-tabControl1.TabPages.Add("Pestanya 3");
+using System;
+using System.Windows.Forms;
 
-private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+namespace EjemploTabControl
 {
-  MessageBox.Show("Pestanya seleccionada: " + tabControl1.SelectedIndex.ToString());
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Agrega algunas pestañas al TabControl
+            tabPage1.Text = "Pestaña 1";
+            tabPage2.Text = "Pestaña 2";
+            tabPage3.Text = "Pestaña 3";
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Maneja el evento de cambio de índice del TabControl
+            MessageBox.Show("Se ha cambiado a la pestaña: " + tabControl1.SelectedTab.Text);
+        }
+    }
 }
